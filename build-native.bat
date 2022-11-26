68c3f8536a86198c6110cbe09d7fc48e0fb9c59e
Samuel Chan <emschan@live.cn>
2022-11-26 23:32:29 +0800

    Solve the native-image issue for getting DNS server lists

--- build-native.bat
+++ build-native.bat
@@ -1,3 +1,5 @@
 @call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-native-image --enable-preview --verbose -O3 -jar target\kay-1.0-jar-with-dependencies.jar -o kay -H:+ReportExceptionStackTraces --allow-incomplete-classpath --no-fallback --initialize-at-run-time=io.netty.resolver.dns.DnsNameResolver,io.netty.resolver.HostsFileEntriesResolver,io.netty.resolver.dns.DefaultDnsServerAddressStreamProvider,io.netty.resolver.dns.DnsServerAddressStreamProviders$DefaultProviderHolder,io.netty.channel.epoll.Epoll,io.netty.channel.epoll.Native,io.netty.channel.unix.Errors --initialize-at-build-time=io.netty
+native-image --enable-preview --add-exports java.base/sun.net.dns=ALL-UNNAMED --verbose -O3 -jar target\kay-1.0-jar-with-dependencies.jar -o kay -H:+ReportExceptionStackTraces --no-fallback --initialize-at-
+run-time=io.netty.handler.ssl.BouncyCastleAlpnSslUtils,io.netty.channel.epoll.Epoll,io.netty.channel.epoll.Native,io.netty.channel.unix.Errors,io.netty.resolver.dns.DefaultDnsServerAddressStreamProvider,sun.net.dns.ResolverConfigura
+tionImpl
 @pause
