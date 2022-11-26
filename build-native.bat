ca4a79487c24c0c5b6093d8b21d995edb645827b
Samuel Chan <emschan@live.cn>
2022-11-26 23:39:10 +0800

    Solve the native-image issue for getting DNS server lists

--- build-native.bat
+++ build-native.bat
@@ -1,6 +1,5 @@
 @call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-native-image --verbose -O3 -jar target\kay-1.0-jar-with-dependencies.jar -o kay ^
--H:+ReportExceptionStackTraces --no-fallback ^
---initialize-at-run-time=io.netty.channel.epoll.Epoll,io.netty.channel.epoll.Native,^
-io.netty.channel.unix.Errors
+native-image --enable-preview --add-exports java.base/sun.net.dns=ALL-UNNAMED --verbose -O3 -jar target\kay-1.0-jar-with-dependencies.jar -o kay -H:+ReportExceptionStackTraces --no-fallback --initialize-at-
+run-time=io.netty.handler.ssl.BouncyCastleAlpnSslUtils,io.netty.channel.epoll.Epoll,io.netty.channel.epoll.Native,io.netty.channel.unix.Errors,io.netty.resolver.dns.DefaultDnsServerAddressStreamProvider,sun.net.dns.ResolverConfigura
+tionImpl
 @pause
